c9b1f1259ea25a47637ba38531b37a5b6aff7c87
VikyArthya <vikyarthya1945@gmail.com>
2025-04-03 21:46:51 +0700

    fix

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -27,8 +27,8 @@ const page = async ({
           <h1 className="text-5xl mb-5">{post.title}</h1>
           <div className="flex items-center">
             <p className="text-sm text-gray-500">By</p>
-            <div className="w-5 h-5 ml-3 mr-2 rounded-full bg-contain" style={{ backgroundImage: `url(${post.author.image})` }}></div>
-            <p className="text-sm text-gray-500">{post.author.name}</p>
+            <div className="w-5 h-5 ml-3 mr-2 rounded-full bg-contain" style={{ backgroundImage: `url(${post.author?.image})` }}></div>
+            <p className="text-sm text-gray-500">{post.author?.name}</p>
           </div>
           <p className="mt-4">{post.content}</p>
         </>
